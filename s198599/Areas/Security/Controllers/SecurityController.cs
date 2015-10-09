d3403946ead357e6d42e71ff6b6513ad927e5c0e
espenbz@gmail.com <espenbz@gmail.com>
2015-10-09 12:07:20 +0200

    Virker som endring til SecurityController fungerer bra.

--- s198599/Areas/Security/Controllers/SecurityController.cs
+++ s198599/Areas/Security/Controllers/SecurityController.cs
@@ -1,4 +1,5 @@
 ﻿using BLL.AuthenticationServices;
+using BLL.BussinessObjectOperations;
 using BLL.ShoppingCartService;
 using BOL.Models;
 using s198599.Controllers;
@@ -6,44 +7,64 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Security;
 
 namespace s198599.Areas.Security.Controllers
 {
     [AllowAnonymous]
-    public class LoginController : BaseController
+    public class SecurityController : BaseController
     {
-        // GET: Security/Login
-        public ActionResult Index()
+        // GET: Security/Register
+        public ActionResult Login()
         {
-            
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult SignIn(User user)
+        public ActionResult Register([Bind(Include = "FirstName,LastName,Email,Password,Telephone,Address,PostCode,PostAddress")] User User)
         {
-            try {
+
+            if (ModelState.IsValid)
+            {
+                new ComplexTransactions().createUserAndUpdateRole(User);
+                return RedirectToAction("Login", "Security", new { area = "Security" });
+            }
+            return View(User);
+        }
+
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user)
+        {
+            try
+            {
 
                 if (PasswordUtility.CheckUsedPasswordAgainstHashed(user.Email, user.Password))
                 {
                     FormsAuthentication.SetAuthCookie(user.Email, false);
-                   
+
                     Session.Add("UserId", user.UserID);
                     return RedirectToAction("Index", "Home", new { area = "Common" });
                 }
                 else
                 {
                     TempData["LoginMsg"] = "Login failed, please try again.";
-                    return RedirectToAction("Index");
+                    return SetSessionMessage(View(user), "fail", "You did not supply correct information. Please try again.");
                 }
-            }catch(Exception e)
+            }
+            catch (Exception e)
             {
                 TempData["LoginMsg"] = "Login failed " + e.Message;
-                return RedirectToAction("Index");
+                return SetSessionMessage(RedirectToAction("Login"), "success", "You are now logged in.");
             }
         }
 
@@ -53,7 +74,7 @@ namespace s198599.Areas.Security.Controllers
             ShoppingCartManager.getInstance().deleteShopppingCart(sessionID);
             FormsAuthentication.SignOut();
             Session.Abandon();
-            return SetSessionMessage(RedirectToAction("Index", "Home", new { area = "Common" }),"success","You are now logged out!");
+            return SetSessionMessage(RedirectToAction("Index", "Home", new { area = "Common" }), "success", "You are now logged out!");
         }
     }
 }
